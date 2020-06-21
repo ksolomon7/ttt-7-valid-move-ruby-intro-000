68808889fb853719e35e0a04b0f1763553b27c4a
Kalkidan Taye <kalkidan.solomon2@gmail.com>
2020-06-21 01:48:32 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board,index)
   if position_taken?(board,index)==false && index == be_between?(0,8)
     true
-  elsif position_taken?(board,index)==true || board[index] != be_between?(0,8)
+  elsif position_taken?(board,index)==true && board[index] != be_between?(0,8)
     false
   end
 end
